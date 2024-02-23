e725e7fd2a7cca0d2402b6b62e9685d31d3dbf37
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-02-23 18:34:58 +0100

    header finito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('page-title') | Laravel Start 1</title>
+        <title>Laravel Comics</title>
 
-        {{-- Stili nell'head --}}
         <link rel="stylesheet" href="css/style.css">
-
-        {{-- <!-- Includiamo gli assets con la direttiva @vite --> --}}
-        @vite('resources/js/app.js')
-
-        @yield('head-imports')
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        @Vite('resources/js/app.js')
     </head>
     <body>
 
@@ -24,7 +20,5 @@
 
         @include('partials.footer')
 
-        {{-- JS da importare nel body --}}
-        @yield('body-imports')
     </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -55,6 +55,34 @@
 
 <header>
     <nav>
-      
+      <div class=" container">
+        
+        <div class="box-header">
+            <div class="logo-header">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+        
+            <div class="navbar-header">
+                <ul class="d-flex">
+                    @foreach ($links as $link)
+
+                    <li class=" list-unstyled m-2">
+                       <a class=" text-decoration-none item" href="">{{$link['label']}}</a>
+                    </li>
+
+                   @endforeach
+                </ul>
+            </div>
+            <div class="input-header">
+              <input class=" border-0" type="text" placeholder="search"><span><i class="fa-solid fa-magnifying-glass"></i></span>
+            </div>
+        </div>
+      </div>
     </nav>
+    <div class="bottom-header">
+        <div class=" container  position-relative ">
+            <button class="blu-button">CURRENT SERIES</button>
+        </div>
+        
+    </div>
 </header>
